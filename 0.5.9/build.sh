707e7654893100bfa03960c00c1449d1737ad567
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-03-05 01:49:25 -0500

    - Add newline at the end of the file. - Include newline at the end of the file.

--- 0.5.9/build.sh
+++ 0.5.9/build.sh
@@ -7,3 +7,5 @@ IMAGE=apptainer-lowcharts-0.5.9.sif
 DEFINITION=recipe.def
 
 apptainer build -F $IMAGE $DEFINITION
+
+python3 -c 'import random; [print(random.normalvariate(5, 5)) for _ in range(100000)]' | ./apptainer-lowcharts-0.5.9.sif hist:
